9ecbbfc1289a8770296916865f79c092a1d88d5e
OlenaKut <105447315+OlenaKut@users.noreply.github.com>
2025-05-14 22:28:15 +0200

    Created image

--- test_app.py
+++ test_app.py
@@ -1,6 +1,27 @@
+import unittest
 from app import app
 
-def test_status():
-    client = app.test_client()
-    response = client.get('/books')
-    assert response.status_code in [200, 500]  # 500 = if DB not yet initialized
+class SimpleApiTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home_route(self):
+        res = self.client.get("/")
+        self.assertEqual(res.status_code, 200)
+
+    def test_books_get(self):
+        res = self.client.get("/books")
+        self.assertEqual(res.status_code, 200)
+        self.assertIsInstance(res.get_json(), list)
+
+    def test_create_book(self):
+        res = self.client.post("/books", json={
+            "title": "Testbok",
+            "author": "Olena",
+            "year": 2023
+        })
+        self.assertEqual(res.status_code, 201)
+        self.assertIn("title", res.get_json())
+
+if __name__ == '__main__':
+    unittest.main()
